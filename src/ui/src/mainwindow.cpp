3a69a89a369fa49c8c04e428e54abe1b960602a0
Gleb <gpbakanov@miem.hse.ru>
2021-10-28 01:09:01 +0300

    Corrected correction of window in window widget for renderer

--- src/ui/src/mainwindow.cpp
+++ src/ui/src/mainwindow.cpp
@@ -3,22 +3,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget* parent) :
-    QMainWindow(parent),
-    ui_(new Ui::MainWindow){
-    ui_->setupUi(this);
-    mdi_area_ = new QMdiArea(this);  // init QMdiArea
-    // Define scrollbars
-    mdi_area_->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-    mdi_area_->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-    // Set up Mdi Area as a central widget
-    setCentralWidget(mdi_area_);
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent), ui_(new Ui::MainWindow) {
+  ui_->setupUi(this);
+  mdi_area_ = new QMdiArea(this);
+  mdi_area_->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+  mdi_area_->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+  setCentralWidget(mdi_area_);
 }
 
-
-
-MainWindow::~MainWindow(){
-    delete ui_;
+MainWindow::~MainWindow() {
+  delete ui_;
 }
 
 void MainWindow::LoadFile(const std::string& file_name) {
@@ -30,19 +25,15 @@ void MainWindow::OnLoadFile(const std::string& file_name) {
   LoadFile(file_name);
 }
 
-void MainWindow::OnActionAddWindowTriggered(){
-    // Create a widget, that will be a window
-    QWidget* widget = new QWidget(mdi_area_);
-    // Add a position
-    QGridLayout *grid_layout = new QGridLayout(widget);
-    widget->setLayout(grid_layout);
-    // Add a label
-    QLabel *label = new QLabel("Hello, I am sub window!!!", widget);
-    grid_layout->addWidget(label);
-    // Add widget as a subwindow Mdi Area
-    mdi_area_->addSubWindow(widget);
-    // Set up a window title
-    widget->setWindowTitle("Sub Window");
-    // Showing widget
-    widget->show();
+/*
+void MainWindow::OnActionAddWindowTriggered() {
+  QWidget* widget = new QWidget(mdi_area_);
+  QGridLayout* grid_layout = new QGridLayout(widget);
+  widget->setLayout(grid_layout);
+  QLabel* label = new QLabel("Hello, I am sub window!!!", widget);
+  grid_layout->addWidget(label);
+  mdi_area_->addSubWindow(widget);
+  widget->setWindowTitle("Sub Window");
+  widget->show();
 }
+*/
--- src/ui/src/mainwindow.h
+++ src/ui/src/mainwindow.h
@@ -10,29 +10,27 @@ namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
  public:
-    static MainWindow& instance() { static MainWindow instance; return instance; }
+  static MainWindow& instance() { static MainWindow instance; return instance; }
   void LoadFile(const std::string& file_name);
-    ~MainWindow();
+  ~MainWindow();
  signals:
     //todo
  public slots:
   void OnLoadFile(const std::string& file_name);
 
- private slots:
-  void OnActionAddWindowTriggered();
+// private slots:
+//  void OnActionAddWindowTriggered();
 
  private:
   Core& core() { return Core::instance(); }
-  explicit MainWindow(QWidget *parent = nullptr);
+  explicit MainWindow(QWidget* parent = nullptr);
   std::unique_ptr<RendererWidget> render_widget_;
   std::unique_ptr<VisualisationOptionsWidget> visualisation_options_;
-  Ui::MainWindow *ui_;
-  QMdiArea * mdi_area_;
-
+  Ui::MainWindow* ui_;
+  QMdiArea* mdi_area_;
 };
 
 
