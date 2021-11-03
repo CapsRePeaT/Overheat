4e588d42b9f3ad55959c857b0b6129dab985f338
Alexey Zaytsev <adzaytsev_1@miem.hse.ru>
2021-11-03 23:17:44 +0300

    Added orthographic camera

---
+++ src/renderer/src/renderer/orthographic_camera.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+// TODO: Current way to handle zoom makes objects proportional to screen height
+
+class OrthographicCamera {
+ public:
+	OrthographicCamera() = default;
+	OrthographicCamera(float aspect_ratio, float zoom,
+	                   std::pair<float, float> depth_bounds = {1.0f, 1.0f});
+	OrthographicCamera(float left, float right, float bottom, float top,
+	                   float near = -1.0f, float far = 1.0f);
+	void SetProjection(float left, float right, float bottom, float top,
+	                   float near = -1.0f, float far = 1.0f);
+
+	[[nodiscard]] const glm::vec3& position() const { return position_; }
+	[[nodiscard]] float rotation() const { return rotation_; }
+
+	void SetPosition(const glm::vec3& position) {
+		position_ = position;
+		RecalculateViewMatrix();
+	}
+	void SetRotation(float rotation) {
+		rotation_ = rotation;
+		RecalculateViewMatrix();
+	}
+	void Zoom(float delta) {
+		zoom_level_ *= delta;
+		RecalculateProjectionMatrix();
+	}
+	void SetAspectRatio(float ratio) {
+		aspect_ratio_ = ratio;
+		RecalculateProjectionMatrix();
+	}
+	[[nodiscard]] const glm::mat4& viewMatrix() const { return view_matrix_; }
+	[[nodiscard]] const glm::mat4& projectionMatrix() const {
+		return projection_matrix_;
+	}
+	[[nodiscard]] const glm::mat4& viewProjectionMatrix() const {
+		return view_projection_matrix_;
+	}
+	[[nodiscard]] float zoom_level() const { return zoom_level_; }
+	[[nodiscard]] float aspect_ratio() const { return aspect_ratio_; }
+
+ private:
+	void RecalculateViewMatrix();
+	void RecalculateProjectionMatrix();
+	glm::mat4 view_matrix_ = glm::mat4(1.0f);
+	glm::mat4 projection_matrix_ = glm::mat4(1.0f);
+	glm::mat4 view_projection_matrix_ = glm::mat4(1.0f);
+
+	glm::vec3 position_ = {0.0f, 0.0f, 0.0f};
+	float rotation_ = 0.0f;
+
+	float aspect_ratio_ = 1.0f;
+	float zoom_level_ = 1.0f;
+
+	float near_ = -1.0f;
+	float far_ = 1.0f;
+};
