89d9714e53659927b6d2821b8b3ad54cceec2aaa
Gleb <gpbakanov@miem.hse.ru>
2021-12-06 02:14:40 +0300

    Started to create ui for Options section

--- src/ui/src/visualization_options.cpp
+++ src/ui/src/visualization_options.cpp
@@ -1,10 +1,19 @@
 #include "visualization_options.h"
-
 #include "ui_visualization_options.h"
 
+#include <QColorDialog>
+
 VisualizationOptionsWidget::VisualizationOptionsWidget(QWidget* parent)
-    : QDockWidget(parent), ui_(new Ui::VisualizationOptions) {
+		: QDockWidget(parent), ui_(new Ui::VisualizationOptionsWidget) {
 	ui_->setupUi(this);
+	connect(ui_->cold_btn, &QAbstractButton::clicked, this,
+	        [=]() { QColor color = QColorDialog::getColor(Qt::blue, this); 
+	                assert(color.isValid() && "Color is not valid");
+					        QString qss = QString("background-color: %1").arg(color.name());
+									ui_->cold_btn->setStyleSheet(qss);
+									visualization_options_.min_temp_color = color; 
+									emit VisualizationOptionsChanged(visualization_options_);
+		             });
 }
 
 VisualizationOptionsWidget::~VisualizationOptionsWidget() { delete ui_; }
