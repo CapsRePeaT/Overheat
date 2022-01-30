ed6a61c84905f53aca3eb29a9e59ff5aa6838a6c
Alexey Zaytsev <adzaytsev_1@miem.hse.ru>
2022-01-31 01:17:27 +0300

    Added (forgotten) ICamera

---
+++ src/renderer/src/renderer/i_camera.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glm/ext/quaternion_geometric.hpp>
+#include <glm/ext/vector_float3.hpp>
+#include "renderer/scene_object.h"
+
+class ICamera : public SceneObject {
+ public:
+	inline void SetScale(glm::vec3 /*scale*/) final {}
+	inline void ApplyScale(glm::vec3 /*scale*/) final {}
+	inline void ApplyScale(float /*scale*/) final {}
+
+	[[nodiscard]] glm::mat4 transform() const override {
+		const glm::mat4 rot = glm::mat4_cast(rotation_);
+		return glm::translate(glm::mat4(1.0f), position_) * rot;
+	}
+
+	virtual void LookAt(glm::vec3 point_to_view) = 0;
+	// Zoom view relative to current zoom level
+	virtual void Zoom(float delta) = 0;
+	[[nodiscard]] virtual float zoom_level() const = 0;
+	virtual void SetAspectRatio(float ratio) = 0;
+	[[nodiscard]] virtual float aspect_ratio() const = 0;
+	
+	[[nodiscard]] virtual const glm::mat4& viewMatrix() const = 0;
+	[[nodiscard]] virtual const glm::mat4& projectionMatrix() const = 0;
+	[[nodiscard]] virtual const glm::mat4& viewProjectionMatrix() const = 0;
+	
+	[[nodiscard]] virtual inline glm::vec3 forwardVec() {
+	}
+};
