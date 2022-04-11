b6f22f39034c9976ea2db3929475307d071ae0cd
Peter Tsygikalo <pntsygikalo@miem.hse.ru>
2022-04-11 03:07:49 +0300

    codereview fixes 2

--- src/ui/src/mainwindow.cpp
+++ src/ui/src/mainwindow.cpp
@@ -82,8 +82,8 @@ void MainWindow::GetFilesAndLoad(const GeometryType type) {
 	QDir trm_dir = QFileInfo(trm_file).absoluteDir();
 	QString absolute_trm_dir = trm_dir.absolutePath();
 	const QString t2d_file = QFileDialog::getOpenFileName(this, tr("Open T2D File"),
-																												absolute_trm_dir,
-																											  tr("geom (*.txt *.T2D);; ALL (*.*)"));
+	                                                      absolute_trm_dir,
+		                                                    tr("geom (*.txt *.T2D);; ALL (*.*)"));
 	if (trm_file.length() && t2d_file.length()) {
 		try {
 			LoadFile(trm_file.toStdString(), t2d_file.toStdString(), type);
