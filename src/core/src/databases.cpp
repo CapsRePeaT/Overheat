e6792b361442ef6da0aac11ed98bc06b23110e4f
Peter Tsygikalo <pntsygikalo@miem.hse.ru>
2022-04-24 18:58:34 +0300

    added getters for global ids for side widgets, added inprovements to layers and heatmap class

--- src/core/src/databases.cpp
+++ src/core/src/databases.cpp
@@ -14,10 +14,29 @@ MetadataPack FileRepresentation::GetShapeMetadata(const ShapeId /*id*/) const {
 	return result;
 }
 
+GobalIds FileRepresentation::GetAllLayerIds() const { 
+	GobalIds result; 
+	for (const auto& layer : layers_) 
+		result.push_back(layer.id());
+	return result;
+}
+
+GobalIds FileRepresentation::GetAllShapeIdsOfLayer(GlobalId layer_id) const {
+	assert(layer_id.type() == InstanceType::Layer);
+	GobalIds result;
+	for (const auto& shape : geom_storage_.shapes()) 
+		if (layer_id.id() == shape->layer_id())
+			result.push_back(shape->id());
+	return result;
+}
+
 HeatmapStorage::HeatmapStorage(std::vector<float> x_steps_mv,
                                std::vector<float> y_steps_mv,
-                               const std::vector<float>& temperature)
-		: x_steps_(std::move(x_steps_mv)), y_steps_(std::move(y_steps_mv)) {
+                               const std::vector<float>& temperature,
+                               Box3D design_borders)
+		: x_steps_(std::move(x_steps_mv)),
+			y_steps_(std::move(y_steps_mv)), 
+		  design_borders_(std::move(design_borders)) {
 	const size_t heatmap_resolution = x_steps_.size() * y_steps_.size();
 	
 	layers_count_ = temperature.size() / heatmap_resolution;
