93800d566199cd3b10a9840e2d751240885d0e39
AISavinov <ansav322@gmail.com>
2022-05-24 15:29:59 +0300

    fix 2d shapes

--- src/readers/src/solver2d/solver2d_data_provider.cpp
+++ src/readers/src/solver2d/solver2d_data_provider.cpp
@@ -37,15 +37,15 @@ Solver2dDataProvider::Solver2dDataProvider(const Solver2d_TRM& geom,
 void Solver2dDataProvider::load_geometry(const Solver2d_TRM& data) {
 	geometry_.Clear();
 	// read body
-	build_layers_shapes(data.size_.width, data.size_.length, data.l1_, geometry_);
-	build_layers_shapes(data.size_.width, data.size_.length, data.l2_, geometry_);
+	//build_layers_shapes(data.size_.width, data.size_.length, data.l1_, geometry_);
+	//build_layers_shapes(data.size_.width, data.size_.length, data.l2_, geometry_);
 
 	constexpr auto thickness_2d = 0.01f;  // for elements without thickness
-	for (const auto& element : data.elements_) {
-		const auto coordinates = element.box_;
-		Box3D box{{{coordinates.x1_, coordinates.x2_},
-		           {coordinates.y1_, coordinates.y2_},
-		           {0.0f, thickness_2d}}};
+  for (const auto& element : data.elements_)
+    for (const auto& coordinates : element.boxes_) {
+      Box3D box{{{coordinates.x1_, coordinates.x2_},
+                 {coordinates.y1_, coordinates.y2_},
+                 {0.0f, thickness_2d}}};
 		geometry_.AddShape(std::make_unique<BasicShape>(getNewShapeId(), 0, box));
 	}
 }
