102669b1ad9f52807a6a003c502ddd9df3237d4a
Petr Tsygikalo <tsygikalo.peter@yandex.ru>
2023-03-12 04:00:57 +0300

    added hypre example, adapted for 1 CPU

--- src/fem_solver/src/fem_solver.cpp
+++ src/fem_solver/src/fem_solver.cpp
@@ -49,7 +49,7 @@ void FemSolver::Solve(FileRepresentation& file_rep) {
 	std::cout << "Adding element contribution to main matrix fineshed, it took "
 		<< std::chrono::duration_cast<std::chrono::seconds>(timer_matrix_filling_fin - timer_cutter_fin)
 		<< " seconds." << std::endl;
-	const auto heatmap = main_matrix.SolveBoostLuFactorisation();
+	const auto heatmap = main_matrix.SolveCholeskyFactorisation();
 	auto timer_matrix_fin = std::chrono::high_resolution_clock::now();
 	std::cout << "Main matrix soliving fineshed, it took "
 		<< std::chrono::duration_cast<std::chrono::seconds>(timer_matrix_fin - timer_matrix_filling_fin)
